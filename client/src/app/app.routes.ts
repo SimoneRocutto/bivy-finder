2225bc2dbd48fee8ab7fcea2e4ddf8d1cadfc809
Simone Rocutto <simonerocutto@gmail.com>
2024-07-19 10:41:04 +0200

    client - employees example removed

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,5 @@
 import { BivouacsMapComponent } from "./features/bivouacs-map/bivouacs-map.component";
 import { Routes } from "@angular/router";
-import { EmployeesListComponent } from "./features/employees/employees-list/employees-list.component";
-import { AddEmployeeComponent } from "./features/employees/add-employee/add-employee.component"; // <-- add this line
-import { EditEmployeeComponent } from "./features/employees/edit-employee/edit-employee.component"; // <-- add this line
 import { LoginComponent } from "./features/auth/login/login.component";
 import { HomeComponent } from "./features/home/home.component";
 import { UserSignUpComponent } from "./features/auth/user-sign-up/user-sign-up.component";
@@ -12,13 +9,6 @@ export const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "bivouacs-map", component: BivouacsMapComponent },
   { path: "admin-dashboard", component: AdminDashboardComponent },
-  {
-    path: "employees",
-    component: EmployeesListComponent,
-    title: "Employees List",
-  },
-  { path: "new-employee", component: AddEmployeeComponent },
-  { path: "edit/:id", component: EditEmployeeComponent },
   { path: "login", component: LoginComponent },
   { path: "sign-up", component: UserSignUpComponent },
 ];
