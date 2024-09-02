f4373e56e9ef24bcbe16844ad9ec77a4887a3680
Simone Rocutto <simonerocutto@gmail.com>
2024-09-02 15:47:43 +0200

    client - production config

---
+++ client/src/environments/environment.production.ts
@@ -0,0 +1,6 @@
+import { Environment } from "../app/types/environment.type";
+
+export const environment: Environment = {
+  baseUrl: "https://mountain-cabins.netlify.app",
+  apiUrl: "https://bivy-finder.onrender.com",
+};
