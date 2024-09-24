f09a3bd9262bf882996cd82b8ca846ad4457e95c
Simone Rocutto <simonerocutto@gmail.com>
2024-09-24 11:31:32 +0200

    client - changed production environment urls

--- client/src/environments/environment.production.ts
+++ client/src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 import { Environment } from "../app/types/environment.type";
 
 export const environment: Environment = {
-  baseUrl: "https://mountain-cabins.netlify.app",
-  apiUrl: "https://bivy-finder.onrender.com",
+  baseUrl: "https://mountain-cabins.xyz",
+  apiUrl: "https://api.mountain-cabins.xyz",
 };
