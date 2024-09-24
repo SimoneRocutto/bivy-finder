ef0ecdb96ed15900514d6d297bb6d9c83bf7d37f
Simone Rocutto <simonerocutto@gmail.com>
2024-09-24 14:30:03 +0200

    client - testing netlify redirects to avoid cookie sameSite problem

--- client/src/environments/environment.production.ts
+++ client/src/environments/environment.production.ts
@@ -2,5 +2,5 @@ import { Environment } from "../app/types/environment.type";
 
 export const environment: Environment = {
   baseUrl: "https://mountain-cabins.xyz",
-  apiUrl: "https://api.mountain-cabins.xyz",
+  apiUrl: "https://mountain-cabins.xyz/api",
 };
