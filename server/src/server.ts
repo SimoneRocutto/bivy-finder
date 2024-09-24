cb4f2cd6931b0ed9f61068f327d3bc015fa92ce5
Simone Rocutto <simonerocutto@gmail.com>
2024-09-24 14:38:52 +0200

    server - trust proxy set to true

--- server/src/server.ts
+++ server/src/server.ts
@@ -77,7 +77,7 @@ export const s3 = new S3Client({
 connectToDatabase(ATLAS_URI)
   .then(async () => {
     const app = express();
-
+    app.set("trust proxy", true);
     // Redis config
     const redisClient = redis.createClient({
       url: REDIS_URL,
