9adabb6ed7891d34510f11d50fff6ea613634a64
Simone Rocutto <simone.rocutto@gmail.com>
2024-10-29 10:52:06 +0100

    server - db seed script for ci-test attempt 7

--- server/db-queries/db-seed.ts
+++ server/db-queries/db-seed.ts
@@ -60,17 +60,13 @@ async function seedDB({ fakeCabinsCount = 500, fakeUsersCount = 500 } = {}) {
     await client.connect();
     console.log("Connected correctly to db");
 
-    const cabinsCollectionTest = client.db(ATLAS_DB).collection("cabins");
-    const check = await cabinsCollectionTest.find().toArray();
-    console.log(check);
-
     const configCollection = client.db(ATLAS_DB).collection("config");
     const config = (await configCollection.find().toArray())[0];
     const allowAccessForTests = config?.allowAccessForTests;
 
-    // if (!allowAccessForTests) {
-    //   throw "Seeding has not been enabled for this database.";
-    // }
+    if (!allowAccessForTests) {
+      throw "Seeding has not been enabled for this database.";
+    }
 
     console.log("Dropping users collection...");
     const usersCollection = client.db(ATLAS_DB).collection("users");
